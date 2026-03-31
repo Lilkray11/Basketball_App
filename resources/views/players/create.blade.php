75d3f6174387e5635a6ec6c9df0d5508c0a0a443
Chuma Brian Okekeze Okoye <chumabrian@Chumas-MacBook-Air.local>
2026-03-31 09:18:04 +0100

    Implemented AJAX player creation using fetch API

--- resources/views/players/create.blade.php
+++ resources/views/players/create.blade.php
@@ -1,6 +1,6 @@
 <h1>Add Player</h1>
 
-<form method="POST" action="/players">
+<form id="playerForm">
     @csrf
 
     <input type="text" name="name" placeholder="Name"><br>
@@ -15,3 +15,28 @@
 
     <button type="submit">Add Player</button>
 </form>
+
+<p id="message"></p>
+
+<script>
+document.getElementById('playerForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let formData = new FormData(this);
+
+    fetch('/players', {
+        method: 'POST',
+        headers: {
+            'X-CSRF-TOKEN': document.querySelector('input[name="_token"]').value
+        },
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('message').innerText = "Player added successfully!";
+    })
+    .catch(error => {
+        console.error(error);
+    });
+});
+</script>
